649ab725d0161f57c2f82691115d8a3d4aa4790d
juan cu <Antony556_04@outlook.com>
2022-09-20 22:52:20 -0500

    cambio la conexion bd, hosting webhost

--- app/config/config.php
+++ app/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = mysqli_connect("bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com", "utur7aovmczn6qtf", "Pp83ju823IBh0nmPhQ9v", "bxgvbqru5r7prhsdpnvr");
+$conexion = mysqli_connect("localhost", "id19600747_root", "XJ2ArN8C0k>gOk{w", "id19600747_sistema");
 
 
 ?>
@@ -12,14 +12,14 @@ $conexion = mysqli_connect("bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com
  * Time: 16:41
  */
 
-define('SERVIDOR', 'bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com');
-define('USUARIO', 'utur7aovmczn6qtf');
-define('PASSWOD', 'Pp83ju823IBh0nmPhQ9v');
-define('BD', 'bxgvbqru5r7prhsdpnvr');
+define('SERVIDOR', 'localhost');
+define('USUARIO', 'id19600747_root');
+define('PASSWOD', 'XJ2ArN8C0k>gOk{w');
+define('BD', 'id19600747_sistema');
 
 //$URL = 'http://localhost/tecnm';
 
-$URL = 'mysql://utur7aovmczn6qtf:Pp83ju823IBh0nmPhQ9v@bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com:3306/bxgvbqru5r7prhsdpnvr';
+//$URL = 'mysql://utur7aovmczn6qtf:Pp83ju823IBh0nmPhQ9v@bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com:3306/bxgvbqru5r7prhsdpnvr';
 
 $servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
 
@@ -32,22 +32,22 @@ try {
 ?>
 
 <?php
-$server = "bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com";
-$user = "utur7aovmczn6qtf";
-$pass = "Pp83ju823IBh0nmPhQ9v";
-$bd = "bxgvbqru5r7prhsdpnvr";
+$server = "localhost";
+$user = "id19600747_root";
+$pass = "XJ2ArN8C0k>gOk{w";
+$bd = "id19600747_sistema";
 
 $conect = new mysqli($server, $user, $pass, $bd);
 ?>
 <?php
-$database = "bxgvbqru5r7prhsdpnvr";
-$user = 'utur7aovmczn6qtf';
-$password = 'Pp83ju823IBh0nmPhQ9v';
+$database = "id19600747_sistema";
+$user = 'id19600747_root';
+$password = 'XJ2ArN8C0k>gOk{w';
 
 
 try {
 
-  $con = new PDO('mysql:host=bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com;dbname=' . $database, $user, $password);
+  $con = new PDO('mysql:host=localhost;dbname=' . $database, $user, $password);
 } catch (PDOException $e) {
   echo "Error" . $e->getMessage();
 }
@@ -55,7 +55,7 @@ try {
 ?>
 <?php
 try {
-  $bdd = new PDO('mysql:host=bxgvbqru5r7prhsdpnvr-mysql.services.clever-cloud.com;dbname=bxgvbqru5r7prhsdpnvr;charset=utf8', 'utur7aovmczn6qtf', 'Pp83ju823IBh0nmPhQ9v');
+  $bdd = new PDO('mysql:host=localhost;dbname=id19600747_sistema;charset=utf8', 'id19600747_root', 'XJ2ArN8C0k>gOk{w');
 } catch (Exception $e) {
   die('Error : ' . $e->getMessage());
 }
